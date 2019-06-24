d7586f1b45dcff3b12753377d2f834ee30880d52
limonadaweb <xan50rus@gmail.com>
2019-06-24 14:09:32 +0300

    Summary

--- js/index.js
+++ js/index.js
@@ -1,85 +1,7 @@
 $(document).ready(function(){
 
-	const wrap = $('#wrapper'),
-			info = $('#information'),
-      btn = $('.menu__order-call, .menu__btn-phone'),
-			infoBtn = $('.orange-btn'),
-      modal = $('.cover, .modal, .content'),
-			submit = $('#content__submit'),
-			infoSubmit = $('#content__info-submit'),
-			infoModal = $('.info-cover, .info-modal, .info-content'),
-			login = $('#content__login'),
-			phone = $('#content__phone'),
-			infoLogin = $('#content__info-login'),
-			infoPhone = $('#content__info-phone'),
-			infoEmail = $('#content__info-email');
-
-btn.on('click', () => {
-  modal.fadeIn();
-	$('.modal, .content').css('display', 'flex');
-});
-
-// close modal
-$('.modal').click( () => {
-  wrap.on('click', function(event) {
-    let select = $('.content');
-    if ($(event.target).closest(select).length)
-      return;
-    modal.fadeOut();
-		login.val('');
-		phone.val('');
-    wrap.unbind('click');
-  });
-});
-
-submit.click( () => {
-	if (login.val() != 0 && phone.val().length === 15 ) {
-		modal.fadeOut();
-		login.val('');
-		phone.val('');
-	} else {
-		alert('Вы не ввели данные!');
-	}
-});
-
-
-infoBtn.on('click', () => {
-  infoModal.fadeIn();
-	$('.info-modal, .info-content').css('display', 'flex');
-});
-
-// close modal
-$('.info-modal').click( () => {
-  info.on('click', function(event) {
-    let select = $('.info-content');
-    if ($(event.target).closest(select).length)
-      return;
-    infoModal.fadeOut();
-		login.val('');
-		infoPhone.val('');
-    info.unbind('click');
-  });
-});
-
-infoSubmit.click( () => {
-	if(infoLogin.val() != 0 && infoPhone.val().length === 15 &&
-		 infoEmail.val().length != 0) {
-		infoModal.fadeOut();
-		infoLogin.val('');
-		infoPhone.val('');
-		infoEmail.val('');
-	} else {
-		alert('Вы не ввели данные!');
-	}
-});
-
-
 $(function(){
-  $("#content__phone").mask("8(999) 999-9999");
-});
-
-$(function(){
-  $("#content__info-phone").mask("8(999) 999-9999");
+  $(".content__phone").mask("+7(999) 999-9999");
 });
 
 	// Находим блок карусели
@@ -157,5 +79,4 @@ $('.mobile-menu__btn').on("click", function() {
   // 	$('html,body').stop().animate({scrollTop: $(this.hash).offset().top + $('#mobile-menu').height()*3.3}, 1000);
   // 	e.preventDefault();
 	// });
-
 });
